4507dde0ce6d207bd4095dd394009cbef6ad2362
Matei Neagu <matn@bendingspoons.com>
2023-01-08 15:55:13 +0200

    feat: add firebase

---
+++ FrontEnd/src/environments/environment.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  firebase: {
+    projectId: 'proiect-angular-1e4df',
+    appId: '1:489886620468:web:800858a21e1a6dae0ea361',
+    storageBucket: 'proiect-angular-1e4df.appspot.com',
+    apiKey: 'AIzaSyCiACnmHrbRA0MnO6BHgYw9dljwhOuffs8',
+    authDomain: 'proiect-angular-1e4df.firebaseapp.com',
+    messagingSenderId: '489886620468',
+  },
+    production: false
+};
